9a4faf087702f506d1ce0eb5e81de129ce6a0fb1
Shawn Klein <zappeln@gmail.com>
2014-10-20 18:24:41 -0500

    commented out user model validations for heroku crash as a potential fix

--- db/migrate/20141006020834_create_users.rb
+++ db/migrate/20141006020834_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string   :username
       t.string   :password
       t.string   :email
-      t.date   :birthdate
+      t.date     :birthdate
       t.boolean  :show_message
 
       t.timestamps
