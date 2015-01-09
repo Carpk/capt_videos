bbc23c01a19a3a0d400bca0888c1497f9a766088
Shawn Klein <zappeln@gmail.com>
2015-01-08 18:31:39 -0600

    set local environment keys to use in fog.rb credentials

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => "",                      # ENV["S3_KEY"],
-    :aws_secret_access_key  => "",   # ENV["S3_SECRET"]
-    :region                 => 'us-west-2'
+    :aws_access_key_id      => ENV["S3_KEY"],
+    :aws_secret_access_key  => ENV["S3_SECRET"],
+    :region                 => ENV["S3_REGION"]
   }
-  config.fog_directory  = ""      # ENV["S3_BUCKET"]
+  config.fog_directory  = ENV["S3_BUCKET"]
 end
