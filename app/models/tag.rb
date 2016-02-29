729670d42dba72f5af6d5da6561536d39ad75f40
brechen <zappeln@gmail.com>
2016-02-29 17:18:52 -0600

    refactor and test Tag class

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,15 +4,10 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :videos, :join_table => "videos_tags"
 
   def self.parse_tags(tags)
-    tags_array = tags.split(",")
-
-    tags_array.map!{|word| word.strip.downcase}
-
-    tags_array.map! do |tag_word|
+    tags.split(",").map do |word|
+      tag_word = word.strip.downcase
       found_tag = Tag.find_by_name(tag_word)
       found_tag ? found_tag : Tag.create({name: tag_word})
     end
-    tags_array
   end
-
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,9 +4,18 @@ describe Tag do
 
   let(:tag) {Tag.last}
 
-  it "should parse a single tag" do
-    tags = 'Epic'
-    expect(Tag.parse_tags(tags).first.class).to eq(Tag)
+  it "should return an array" do
+    tag = 'Epic'
+    expect(Tag.parse_tags(tag).class).to eq(Array)
   end
-  
+
+  it "should parse a single tag" do
+    tag = 'Epic'
+    expect(Tag.parse_tags(tag).first.class).to eq(Tag)
+  end
+
+  it "should parse multiple tags" do
+    tags = 'Epic, GPS'
+    expect(Tag.parse_tags(tags).count).to eq(2)
+  end  
 end
